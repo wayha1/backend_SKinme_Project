8d769a8872d4fbc32bae0349781ccc20683c092e
wayha1 <vehaseng1122@gmail.com>
2024-03-30 16:43:30 +0700

    update

--- app/Http/Controllers/StoreDataController.php
+++ app/Http/Controllers/StoreDataController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\StoreDataResource;
+use App\Models\StoreData;
 use Illuminate\Http\Request;
 
 class StoreDataController extends Controller
 {
-    //
+    public function show( StoreData $product)
+    {
+        return new StoreDataResource($product);
+    }
 }
