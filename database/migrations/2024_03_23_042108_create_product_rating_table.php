d8b1a302c1acfa8d10fd4249456bf559ba2ff9f1
wayha1 <vehaseng1122@gmail.com>
2024-04-19 17:42:11 +0700

    update bit

--- database/migrations/2024_03_23_042108_create_product_rating_table.php
+++ database/migrations/2024_03_23_042108_create_product_rating_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_rating', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
