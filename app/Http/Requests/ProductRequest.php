f4283630ac6e92e414f8bdc8cb4e842c506e918e
ssdn7 <somsodana9@gmail.com>
2024-05-10 14:52:17 +0700

    update

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\UserComments;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProductRequest extends FormRequest
@@ -27,7 +28,10 @@ class ProductRequest extends FormRequest
             'product_price' => ['required', 'numeric'],
             'product_stock' => ['required', 'integer'],
             'product_rating' => ['required', 'numeric', 'min:0', 'max:5'],
+            
             'product_feedback' => ['nullable', 'string', 'max:255'],
+            'product_comment' => UserCommentsRequest::collection($this->whenLoaded('product_comment')),
+            
             'product_image' => ['nullable', 'string', 'max:255'],
             'product_review' => ['nullable', 'string', 'max:255'],
             'product_banner' => ['nullable', 'string', 'max:255'],
