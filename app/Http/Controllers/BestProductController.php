e0290d88c2b86c6e7bd96e7f7312b7bd0eb2e375
ssdn7 <somsodana9@gmail.com>
2024-05-23 15:24:18 +0700

    update best product

--- app/Http/Controllers/BestProductController.php
+++ app/Http/Controllers/BestProductController.php
@@ -6,16 +6,24 @@ use Illuminate\Http\Request;
 
 class BestProductController extends Controller
 {
-    public function index(){
-
-    }
-    public function store(){
-
-    }
-    public function update(){
-
-    }
-    public function destroy(){
-        
-    }
+    public function getBestProducts() {
+        $bestProducts = Product::orderBy('number_of_purchases', 'desc')
+                                ->take(4) // Adjust the number of products you want to fetch
+                                ->get();
+        return response()->json($bestProducts);
 }
+
+}
+    // public function index(){
+
+    // }
+    // public function store(){
+
+    // }
+    // public function update(){
+
+    // }
+    // public function destroy(){
+
+    // }
+
