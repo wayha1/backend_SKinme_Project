8d347cdb176a9fd72c3ecea4213b9df9321a2500
Veha Seng <131443243+wayha1@users.noreply.github.com>
2024-05-29 18:31:02 -0400

    Update ProductRatingController.php

--- app/Http/Controllers/ProductRatingController.php
+++ app/Http/Controllers/ProductRatingController.php
@@ -1,1 +1,37 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\ProductRating;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductRatingController extends Controller
+{
+    public function index(){
+       
+    }
+
+    // Display the specified user history record for the current user
+    public function show()
+    {
+      
+    }
+
+    // Store a newly created user history record for the current user
+    public function store()
+    {
+       
+    }
+
+    // Update the specified user history record for the current user
+    public function update()
+    {
+        
+    }
+    // Remove the specified user history record for the current user
+    public function destroy($id)
+    {
+        
+    }
+}
