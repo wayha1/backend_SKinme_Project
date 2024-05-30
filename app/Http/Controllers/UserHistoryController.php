c895d1b46e8616286a6ae0997015d28ef3794562
wayha1 <vehaseng1122@gmail.com>
2024-05-30 14:19:29 +0700

    last update

--- app/Http/Controllers/UserHistoryController.php
+++ app/Http/Controllers/UserHistoryController.php
@@ -6,7 +6,6 @@ use App\Http\Resources\UserHistoryResource;
 use App\Http\Requests\UserHistoryRequest;
 use App\Http\Requests\UpdateUserHistoryRequest;
 use App\Models\UserHistory;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserHistoryController extends Controller
@@ -14,15 +13,15 @@ class UserHistoryController extends Controller
     // Display a listing of the user history for the current user
     public function index(){
         $user_id = Auth::id();
-        $user_history = UserHistory::with(['users', 'products'])->where('user_id', $user_id)->get();
+        $user_history = UserHistory::with(['user', 'products'])->where('user_id', $user_id)->get();
         return UserHistoryResource::collection($user_history);
     }
 
     // Display the specified user history record for the current user
-    public function show($id)
+    public function show()
     {
         $user_id = Auth::id();
-        $user_history = UserHistory::with(['users', 'products'])->where('user_id', $user_id)->findOrFail($id);
+        $user_history = UserHistory::with(['user', 'products'])->where('user_id', $user_id)->first();
         return new UserHistoryResource($user_history);
     }
 
@@ -42,7 +41,7 @@ class UserHistoryController extends Controller
     {
         $user_id = Auth::id();
         $user_history = UserHistory::where('user_id', $user_id)->findOrFail($id);
-        $data = $request->validate();
+        $data = $request->validated();
         $user_history->update($data);
         return new UserHistoryResource($user_history);
     }
